3e1ce5a0af35f82ad9ebd79c03f84d647a2163ce
fireworks80 <fireworks80@naver.com>
2018-04-16 15:30:04 +0900

    feat: Add function
    - dom return function 생성

--- help.js
+++ help.js
@@ -1,5 +1,13 @@
 var el = function(selector, context) {
-	if (!selector) {
+	if (select.trim().length === 0 || typeof select !== 'string') {
 		return null;
 	}
+
+	if (!context && context.trim().length !== 0 ) { context = document; }
+
+	if (context.nodeType === context.ELEMENT_NODE) {
+		el(String(context));
+	}
+
+	return context.querySelector(selector);
 };
